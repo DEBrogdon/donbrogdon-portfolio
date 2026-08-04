7050a5ed49f15ac0b40dfd7ea769d56214988ea2
Don Brogdon <dbrogdon91@gmail.com>
2026-08-03 20:17:27 -0500

    Add genre prop to GameCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import GameCard from "@/components/GameCard";
 export default function Home() {
     const games = [
-        { name: "Skyrim" },
-        { name: "Civilization V" },
-        { name: "ESO" },
-        { name: "Oblivion" }
+        { name: "Skyrim", genre: "RPG", year: 2011 },
+        { name: "Civilization V", genre: "Strategy", year: 2009 },
+        { name: "ESO", genre: "MMORPG", year: 2014 },
+        { name: "Oblivion", genre: "RPG", year: 2006 },
+        { name: "Palworld", genre: "Survival", year: 2026 }
     ];
 
     return (
         <main>
             {games.map(game => {
-                return <GameCard name={game.name} />;
+                return <GameCard name={game.name} genre={game.genre} year={game.year} />;
             })}
         </main>
     );
--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -1,9 +1,11 @@
 type GameCardProps = {
     name: string;
+    genre: string;
+    year: number;
 }
 
-export default function GameCard({name}: GameCardProps) {
+export default function GameCard({name, genre, year}: GameCardProps) {
     return (
-        <div><h3>{name}</h3></div>
+        <div><h3>{year} {name} - {genre}</h3></div>
     );
 }
