f02fbb1e769c8c98c65dd4a778869e58f9233c7e
Don Brogdon <dbrogdon91@gmail.com>
2026-08-10 16:40:35 -0500

    Added a menu toggler for the button to display bool values

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
 import GameCard from "@/components/GameCard";
+import ToggleMenu from "@/components/ToggleMenu";
 export default function Home() {
-    const games = [
+    //GameCard variables
+    /*const games = [
         { name: "Skyrim", genre: "RPG", year: 2011 },
         { name: "Civilization V", genre: "Strategy", year: 2009 },
         { name: "ESO", genre: "MMORPG", year: 2014 },
         { name: "Oblivion", genre: "RPG", year: 2006 },
         { name: "Palworld", genre: "Survival", year: 2026 }
-    ];
+    ];*/
 
     return (
-        <main>
+        //GameCard Code.
+        /*<main>
             {games.map(game => {
                 return <GameCard name={game.name} genre={game.genre} year={game.year} />;
             })}
+        </main>*/
+
+        <main>
+            <h1>My Portfolio</h1>
+
+            <ToggleMenu />
         </main>
     );
 }
---
+++ components/ToggleMenu.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState } from "react";
+
+export default function ToggleMenu() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <div>
+            <button onClick={() => setMenuOpen(!menuOpen)}>
+                Toggle Menu
+            </button>
+
+            {menuOpen && <p>The menu is open!</p>}
+        </div>
+    );
+}
